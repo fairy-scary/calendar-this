9c190c56304ea38c94b9241e57d481c9f8dad7a0
fairy-scary <ecoautumn@gmail.com>
2021-01-12 17:45:27 -0600

    called TA

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
 from flask import (Blueprint, render_template)
+import os
+import psycopg2
+
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
 
 bp = Blueprint('', __name__)
 
 @bp.route("/")
-def index():
-    return render_template('main.html')
+def main():
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+            ''')
+            rows = cursor.fetchall()
+            print(rows)
+            return render_template('main.html', rows=rows)
