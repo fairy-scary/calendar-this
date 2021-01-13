6d0661f9d650d6b1d07fcd7a23c2e8439f0b2f89
Thanh Nguyen <thanhnguyenttn@gmail.com>
2021-01-13 02:01:22 -0500

    Handling form submit complete

--- app/forms.py
+++ app/forms.py
@@ -9,11 +9,11 @@ from wtforms.validators import DataRequired, Length
 from wtforms.widgets.html5 import DateInput, TimeInput
 
 class AppointmentForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
+    name = StringField("Name", [DataRequired()])
     start_date = DateField("Start Date", [DataRequired()], widget=DateInput())
-    start_time = DateField("Start Time", [DataRequired()], widget=TimeInput())
+    start_time = TimeField("Start Time", [DataRequired()], widget=TimeInput())
     end_date = DateField("End Date", [DataRequired()], widget=DateInput())
-    end_time = DateField("End Time", [DataRequired()], widget=TimeInput())
-    description = TextAreaField('Description', validators=[DataRequired()])
+    end_time = TimeField("End Time", [DataRequired()], widget=TimeInput())
+    description = TextAreaField('Description', [DataRequired()])
     private = BooleanField('Private')
     submit = SubmitField("Create an appointment")
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import (Blueprint, render_template)
+from flask import Blueprint, render_template, redirect
 import os
 import psycopg2
 from app.forms import AppointmentForm
-
+from datetime import datetime
 
 CONNECTION_PARAMETERS = {
     "user": os.environ.get("DB_USER"),
@@ -13,9 +13,36 @@ CONNECTION_PARAMETERS = {
 
 bp = Blueprint('', __name__)
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 def main():
     form = AppointmentForm()
+
+    if form.validate_on_submit():
+        print('inside if statement')
+        params = {
+            'name': form.name.data,
+            'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+            'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+            'description': form.description.data,
+            'private': form.private.data
+            }
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as cursor:
+                    cursor.execute("""
+                        INSERT INTO appointments(
+                            name, start_datetime, end_datetime,
+                            description, private)
+                        VALUES
+                        (%(name)s, %(start_datetime)s, %(end_datetime)s, %(description)s, %(private)s);
+                    """, {"name": params["name"],
+                        "start_datetime": params["start_datetime"],
+                        "end_datetime": params["end_datetime"],
+                        "description": params["description"],
+                        "private": params["private"]
+                        })
+                    return redirect('/')
+
+
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as cursor:
             cursor.execute('''
@@ -27,5 +54,4 @@ def main():
             result = {}
             for row in rows:
                 result[row[0]] = dict(zip(["id", "name", "start_datetime", "end_datetime"], row))
-            print(result)
             return render_template('main.html', rows=result, form=form)
